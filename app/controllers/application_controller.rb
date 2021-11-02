20b0c48ea235a1a7b122fdcbb9a7b9992025532a
ktbg <ktboates@me.com>
2021-11-02 14:46:36 -0400

    authentications controller added, user login and verify routes tested and passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,18 @@ class ApplicationController < ActionController::API
     decoded = JWT.decode(token, SECRET_KEY)[0]
     HashWithIndifferentAccess.new decoded
   end
+
+  def authorize_request
+    header = request.headers['Authorization']
+    token = header.split(' ').last if header
+    begin
+      @payload = decode(token)
+      @current_user = User.find(@payload[:id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post '/auth/login', to: 'authentications#login'
+  get '/auth/verify', to: 'authentications#verify'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
