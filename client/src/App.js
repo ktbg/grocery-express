14f274d26f74792a35f1845a3f1617b77edb9976
ktbg <ktboates@me.com>
2021-11-03 21:01:32 -0400

    login form set up and tested, verify path working

--- client/src/App.js
+++ client/src/App.js
@@ -17,13 +17,13 @@ function App() {
   const [currentUser, setCurrentUser] = useState(null);
   const history = useHistory();
 
-  // useEffect(() => {
-  //   const handleVerify = async () => {
-  //     const userData = await verifyUser();
-  //     setCurrentUser(userData);
-  //   };
-  //   handleVerify();
-  // }, []);
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData);
+    };
+    handleVerify();
+  }, []);
 
   const handleLogin = async (formData) => {
     const userData = await loginUser(formData);
--- client/src/screens/Login/Login.jsx
+++ client/src/screens/Login/Login.jsx
@@ -1,9 +1,64 @@
-const Login = (props) => {
-  return (
-    <div>
-      Login screen
-    </div>
-  )
-}
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { useState } from "react";
+import Button from '../../components/Button/Button'
+import './Login.css'
+import '../../App.css'
 
-export default Login
+const Login = (props) => {
+  const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+      });
+
+  const { email, password } = formData;
+  const { handleLogin } = props;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className="sign-in-container">
+      <h3 className="form-title">Sign In</h3>
+      <p className="form-helper">
+        New to Grocery Express? Sign Up{" "}
+        <span className="chevron">
+          <ChevronRightIcon color="black" />
+        </span>
+      </p>
+      <form
+        className="sign-in-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleLogin(formData);
+        }}
+      >
+        <label>
+          <input
+            type="text"
+            name="email"
+            value={email}
+            placeholder="email"
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="password"
+            onChange={handleChange}
+          />
+        </label>
+        <Button redirectLocation="/" name="Sign In" className="form"/>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
