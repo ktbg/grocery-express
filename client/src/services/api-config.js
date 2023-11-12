47b25eed9b212f13aa2c7665d2d552055810956c
ktbg <ktboates@me.com>
2023-11-12 15:17:27 -0500

    update production be to onRender

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const baseUrl = 
-  process.env.NODE_ENV === 'production' ?
-  "https://banana-scallops-butter-api.herokuapp.com/"
-  : "http://localhost:3000/"
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://grocery-express-backend-v5fu.onrender.com"
+    : "http://localhost:3000/";
 
 const api = axios.create({
   baseURL: baseUrl,
